7ef17e801313754ce2926c6cfa3c11a42459b0cc
Anders Juengst <dersj@live.unc.edu>
2018-10-06 15:57:36 -0400

    fixed logout

--- hacknc/hacknc/urls.py
+++ hacknc/hacknc/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='landing.html')),
-    path('accounts/', include('account.urls')),
+    path('account/', include('account.urls')),
     path('', include('housing.urls')),
 ]
