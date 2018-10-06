ce332a1ff0866ed251a3e853856c5af3ea974be5
Chathan Driehuys <chathan@driehuys.com>
2018-10-06 16:49:53 -0400

    Fix login flow.

--- hacknc/hacknc/settings.py
+++ hacknc/hacknc/settings.py
@@ -131,5 +131,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
--- hacknc/hacknc/urls.py
+++ hacknc/hacknc/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='landing.html')),
+    path('', TemplateView.as_view(template_name='landing.html'), name='home'),
     path('account/', include('account.urls')),
     path('', include('housing.urls')),
 ]
